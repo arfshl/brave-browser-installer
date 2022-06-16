571bb9bf4d8e35579923860ba29754d453e45f42
arfshl <98870352+arfshl@users.noreply.github.com>
2022-06-16 14:51:30 +0700

    Update opensuse.sh

--- script/opensuse.sh
+++ script/opensuse.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
+print "Brave Browser Installer
+print "sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
+sudo zypper addrepo https://brave-browser-rpm-release.s3.brave.com/x86_64/ brave-browser
+sudo zypper refresh
+sudo zypper --non-interactive install brave-browser
+"
+read -p "Press any key to execute script ..."
 sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
 sudo zypper addrepo https://brave-browser-rpm-release.s3.brave.com/x86_64/ brave-browser
 sudo zypper refresh
