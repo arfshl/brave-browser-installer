7afaa82beac4d320c1849476fd2e72fdf98c3e41
arfshl <98870352+arfshl@users.noreply.github.com>
2022-06-16 14:52:12 +0700

    Update redhat.sh

--- script/redhat.sh
+++ script/redhat.sh
@@ -1,4 +1,11 @@
 #!/bin/sh
+print "Brave Browser Installer"
+print "sudo dnf install dnf-plugins-core -y
+sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/x86_64/
+sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
+sudo dnf install brave-browser -y
+"
+read -p "Press any key to execute script ..."
 sudo dnf install dnf-plugins-core -y
 sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/x86_64/
 sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
