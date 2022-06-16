c4d62a7544519f51c7a3d85619e15662ff74a559
arfshl <98870352+arfshl@users.noreply.github.com>
2022-06-16 14:49:23 +0700

    Update debian.sh

--- script/debian.sh
+++ script/debian.sh
@@ -1,4 +1,13 @@
 ﻿#!/bin/sh
+print "Brave Browser Installer"
+print "sudo apt-get update
+sudo apt-get install -y curl apt-transport-https
+sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
+echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
+sudo apt-get update
+sudo apt-get install -y brave-browser
+"
+read -p "Press any key to execute script ..."
 sudo apt-get update
 sudo apt-get install -y curl apt-transport-https
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
