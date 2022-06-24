88335f27b9b7d51fbbed54b0e753da0412625e37
arfshl <98870352+arfshl@users.noreply.github.com>
2022-06-24 17:37:53 +0700

    Update arch.sh

--- arch.sh
+++ arch.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 echo "Brave browser installer"
-echo "sudo pacman -S base-devel git
-git clone https://aur.archlinux.org/brave-bin.git
-cd brave-bin
-sudo makepkg -si"
-read -p "Press any key to install..."
 sudo pacman -S base-devel git
 git clone https://aur.archlinux.org/brave-bin.git
 cd brave-bin
