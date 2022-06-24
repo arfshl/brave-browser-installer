ca4097dceaec6084731f170d0e2a33b52aa59fb2
arfshl <98870352+arfshl@users.noreply.github.com>
2022-06-24 15:45:07 +0700

    Update arch.sh

--- arch.sh
+++ arch.sh
@@ -4,7 +4,7 @@ echo "sudo pacman -S base-devel git
 git clone https://aur.archlinux.org/brave-bin.git
 cd brave-bin
 sudo makepkg -si"
-read -p "Press any key to execute script ..."
+read -p "Press any key to install..."
 sudo pacman -S base-devel git
 git clone https://aur.archlinux.org/brave-bin.git
 cd brave-bin
