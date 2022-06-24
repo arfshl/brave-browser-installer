7ea99c43726b819b9cc2265783c525bffa7e2846
arfshl <98870352+arfshl@users.noreply.github.com>
2022-06-24 15:46:05 +0700

    Update and rename script/debian.sh to debian.sh

--- debian.sh
+++ debian.sh
@@ -1,6 +1,6 @@
 ﻿#!/bin/sh
-print "Brave browser installer"
-print "sudo apt-get update
+echo "Brave browser installer"
+echo "sudo apt-get update
 sudo apt-get install -y curl apt-transport-https
 sudo curl -fsSLo /usr/share/keyrings/brave-browser-archive-keyring.gpg https://brave-browser-apt-release.s3.brave.com/brave-browser-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/brave-browser-archive-keyring.gpg arch=amd64] https://brave-browser-apt-release.s3.brave.com/ stable main"|sudo tee /etc/apt/sources.list.d/brave-browser-release.list
