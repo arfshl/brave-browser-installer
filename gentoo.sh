a02fe197e1f4e28924d1c4ab58df1ee188a0031c
arfshl <98870352+arfshl@users.noreply.github.com>
2022-06-27 18:52:53 +0700

    Update gentoo.sh

--- gentoo.sh
+++ gentoo.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
+echo "Brave browser installer"
 emerge --ask app-eselect/eselect-repository dev-vcs/git 
+eselect repository list | grep brave [51] brave-overlay
 eselect repository enable brave-overlay
 emaint sync -r brave-overlay
 emerge --ask brave-bin
